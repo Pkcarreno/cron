0dccd196cc9d9af71623efafc1a2b75b43b51bfb
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-02-19 14:36:06 -0400

    chore: automate version sync in app.json

---
+++ scripts/sync-version.js
@@ -0,0 +1,8 @@
+/* oxlint-disable no-nodejs-modules */
+import fs from 'node:fs';
+import pkg from '@/package.json';
+import app from '@/app.json';
+
+app.expo.version = pkg.version;
+/* oxlint-disable jest/require-hook */
+fs.writeFileSync('./app.json', `${JSON.stringify(app, null, 2)}\n`);
