a0a53d6fd9a9508325e33242487783c478930583
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-02-19 14:50:20 -0400

    fix: take control over splash-screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,17 @@ import { StyleSheet } from 'react-native';
 import { colors } from '@/helpers/colors';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+/* oxlint-disable jest/require-hook */
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
   return (
     <>
       {/* oxlint-disable react/style-prop-object */}
