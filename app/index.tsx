f7fc5cd3736b24229845793f8a426f75e0ff1fe0
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-02-21 13:32:59 -0400

    feat: update ui to minimal preview timer behavior

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,128 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from '@/helpers/colors';
 import { Link } from 'expo-router';
+import type { TimerConfig } from '@/helpers/timer/factory';
+import { TimerMode } from '@/helpers/timer/factory';
+import { serializeTimerConfig } from '@/helpers/timer/utils/config-serializer';
+import { convertTimeToMs } from '@/helpers/timer/utils/formatter';
+
+const emomConfig: TimerConfig = {
+  mode: TimerMode.EMOM,
+  preparationMs: convertTimeToMs(0, 10),
+  totalRounds: 3,
+};
+const tabataConfig: TimerConfig = {
+  mode: TimerMode.TABATA,
+  preparationMs: convertTimeToMs(0, 10),
+};
+const onOffConfig: TimerConfig = {
+  mode: TimerMode.ON_OFF,
+  preparationMs: convertTimeToMs(0, 10),
+  restMs: convertTimeToMs(0, 20),
+  totalRounds: 3,
+  workMs: convertTimeToMs(0, 30),
+};
+const amrapConfig: TimerConfig = {
+  durationMs: convertTimeToMs(4, 0),
+  mode: TimerMode.AMRAP,
+  preparationMs: convertTimeToMs(0, 10),
+};
+const forTimeConfig: TimerConfig = {
+  mode: TimerMode.FOR_TIME,
+  preparationMs: convertTimeToMs(0, 10),
+  timecapMs: convertTimeToMs(3, 0),
+};
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Estas en el index</Text>
-      <Link href="/timer" style={styles.link}>
-        Ir al timer
-      </Link>
+      <Text style={styles.text}>Index Screen</Text>
+      <View style={buttonsStyles.linkWrapper}>
+        <Text style={buttonsStyles.heading}>Jobs</Text>
+        <Link
+          href={{
+            params: serializeTimerConfig(emomConfig),
+            pathname: '/timer',
+          }}
+          style={buttonsStyles.link}
+        >
+          EMOM
+        </Link>
+        <Link
+          href={{
+            params: serializeTimerConfig(tabataConfig),
+            pathname: '/timer',
+          }}
+          style={buttonsStyles.link}
+        >
+          TABATA
+        </Link>
+        <Link
+          href={{
+            params: serializeTimerConfig(amrapConfig),
+            pathname: '/timer',
+          }}
+          style={buttonsStyles.link}
+        >
+          AMRAP
+        </Link>
+        <Link
+          href={{
+            params: serializeTimerConfig(onOffConfig),
+            pathname: '/timer',
+          }}
+          style={buttonsStyles.link}
+        >
+          ON/OFF
+        </Link>
+        <Link
+          href={{
+            params: serializeTimerConfig(forTimeConfig),
+            pathname: '/timer',
+          }}
+          style={buttonsStyles.link}
+        >
+          FOR TIME
+        </Link>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
+    gap: 8,
     justifyContent: 'center',
-  },
-  link: {
-    backgroundColor: colors.neutral[400],
-    borderRadius: 10,
-    color: colors.black,
-    fontFamily: 'Geist',
-    fontWeight: '400',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
+    paddingHorizontal: 16,
   },
   text: {
     color: colors.neutral[400],
     fontFamily: 'Geist',
     fontWeight: '400',
+  },
+});
+
+const buttonsStyles = StyleSheet.create({
+  heading: {
+    color: colors.neutral[500],
+    fontFamily: 'Geist',
+    fontWeight: '400',
+  },
+  link: {
+    backgroundColor: colors.neutral[800],
+    borderRadius: 10,
+    color: colors.neutral[400],
+    fontFamily: 'Geist',
+    fontSize: 18,
+    fontWeight: '400',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    textAlign: 'center',
+  },
+  linkWrapper: {
+    gap: 8,
+    justifyContent: 'center',
+    width: '100%',
   },
 });
--- app/timer.tsx
+++ app/timer.tsx
@@ -1,36 +1,205 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { colors } from '@/helpers/colors';
-import { Link } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
+import { TimerStatus, useTimer } from '@/hooks/use-timer';
+import type { TimerRouteParams } from '@/helpers/timer/utils/config-serializer';
+import { deserializeTimerConfig } from '@/helpers/timer/utils/config-serializer';
 
 export default function TimerScreen() {
+  const rawConfigParams = useLocalSearchParams<TimerRouteParams>();
+  const {
+    minutes,
+    seconds,
+    phase,
+    totalRounds,
+    currentRound,
+    displayTimeMs,
+    flags,
+    modeAbbr,
+    status,
+    start,
+    pause,
+    reset,
+  } = useTimer(deserializeTimerConfig(rawConfigParams));
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Este es el timer</Text>
-      <Link href=".." style={styles.link}>
-        volver a home
-      </Link>
+      <Text style={styles.text}>Timer Screen</Text>
+
+      <View style={timerStyles.container}>
+        <Text style={timerStyles.text}>{minutes}</Text>
+        <Text style={timerStyles.text}>:</Text>
+        <Text style={timerStyles.text}>{seconds}</Text>
+      </View>
+
+      <View style={dataBoxStyles.container}>
+        <Text style={dataBoxStyles.heading}>state</Text>
+        <Text style={dataBoxStyles.text}>{'{'}</Text>
+        <View style={dataBoxStyles.dataWrapper}>
+          <Text style={dataBoxStyles.text}>mode {modeAbbr}</Text>
+          <Text style={dataBoxStyles.text}>status {status}</Text>
+          <Text style={dataBoxStyles.text}>phase {phase}</Text>
+          <Text style={dataBoxStyles.text}>totalRounds {totalRounds}</Text>
+          <Text style={dataBoxStyles.text}>currentRound {currentRound}</Text>
+          <Text style={dataBoxStyles.text}>displayTimeMs {displayTimeMs}</Text>
+        </View>
+        <Text style={styles.text}>{'}'}</Text>
+      </View>
+
+      <View style={dataBoxStyles.container}>
+        <Text style={dataBoxStyles.heading}>flags</Text>
+        <Text style={dataBoxStyles.text}>{'{'}</Text>
+        <View style={dataBoxStyles.dataWrapper}>
+          <Text style={dataBoxStyles.text}>
+            isPreparing {JSON.stringify(flags.isPreparing)}
+          </Text>
+          <Text style={dataBoxStyles.text}>
+            showsPhaseIndicator {JSON.stringify(flags.showsPhaseIndicator)}
+          </Text>
+          <Text style={dataBoxStyles.text}>
+            showsRoundCounter {JSON.stringify(flags.showsRoundCounter)}
+          </Text>
+          <Text style={dataBoxStyles.text}>
+            showsTimeControls {JSON.stringify(flags.showsTimeControls)}
+          </Text>
+        </View>
+        <Text style={styles.text}>{'}'}</Text>
+      </View>
+
+      <View style={buttonsStyles.container}>
+        <View style={buttonsStyles.actionWrapper}>
+          <Pressable onPress={start}>
+            <Text
+              style={
+                status === TimerStatus.READY || status === TimerStatus.PAUSED
+                  ? buttonsStyles.action
+                  : buttonsStyles.actionDisabled
+              }
+              disabled={
+                status === TimerStatus.READY || status === TimerStatus.PAUSED
+              }
+            >
+              Start
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={pause}>
+            <Text
+              style={
+                status === TimerStatus.RUNNING
+                  ? buttonsStyles.action
+                  : buttonsStyles.actionDisabled
+              }
+              disabled={status === TimerStatus.RUNNING}
+            >
+              Pause
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={reset}>
+            <Text
+              style={
+                status === TimerStatus.READY
+                  ? buttonsStyles.actionDisabled
+                  : buttonsStyles.action
+              }
+              disabled={status !== TimerStatus.READY}
+            >
+              Reset
+            </Text>
+          </Pressable>
+        </View>
+        <Link href=".." style={buttonsStyles.link}>
+          Back to Index
+        </Link>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
+    gap: 8,
     justifyContent: 'center',
-  },
-  link: {
-    backgroundColor: colors.neutral[400],
-    borderRadius: 10,
-    color: colors.black,
-    fontFamily: 'Geist',
-    fontWeight: '400',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
+    paddingHorizontal: 16,
   },
   text: {
     color: colors.neutral[400],
     fontFamily: 'Geist',
     fontWeight: '400',
+  },
+});
+
+const timerStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  text: {
+    color: colors.neutral[200],
+    fontFamily: 'Geist Mono',
+    fontSize: 24,
+    fontWeight: '800',
+  },
+});
+
+const dataBoxStyles = StyleSheet.create({
+  container: {
+    paddingVertical: 4,
+  },
+  dataWrapper: {
+    paddingHorizontal: 8,
+  },
+  heading: {
+    color: colors.neutral[400],
+    fontFamily: 'Geist',
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  text: {
+    color: colors.neutral[400],
+    fontFamily: 'Geist Mono',
+    fontWeight: '400',
+  },
+});
+
+const buttonsStyles = StyleSheet.create({
+  action: {
+    backgroundColor: colors.neutral[400],
+    borderRadius: 10,
+    color: colors.black,
+    fontFamily: 'Geist Mono',
+    fontSize: 18,
+    fontWeight: '600',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  actionDisabled: {
+    backgroundColor: colors.neutral[700],
+    borderRadius: 10,
+    color: colors.black,
+    fontFamily: 'Geist Mono',
+    fontSize: 18,
+    fontWeight: '600',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  actionWrapper: {
+    flexDirection: 'row',
+    gap: 4,
+    width: '100%',
+  },
+  container: {
+    gap: 16,
+  },
+  link: {
+    backgroundColor: colors.neutral[800],
+    borderRadius: 10,
+    color: colors.neutral[400],
+    fontFamily: 'Geist Mono',
+    fontSize: 18,
+    fontWeight: '400',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
   },
 });
