802e05562db8f4ba25756190ee27f2f1ebf97c5d
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-02-22 17:35:34 -0400

    fix: remove unused script

--- scripts/sync-version.js
+++
@@ -1,7 +0,0 @@
-/* oxlint-disable no-nodejs-modules */
-import fs from 'node:fs';
-import pkg from '@/package.json';
-import app from '@/app.json';
-
-app.expo.version = pkg.version;
-fs.writeFileSync('./app.json', `${JSON.stringify(app, null, 2)}\n`);
