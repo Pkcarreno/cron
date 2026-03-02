322b32cbdb22ab3f798f69830552b69972235864
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-03-02 10:22:02 -0400

    feat: use custom text and button in index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { colors } from '@/helpers/colors';
+import { StyleSheet, View } from 'react-native';
 import { Link } from 'expo-router';
 import type { TimerConfig } from '@/helpers/timer/factory';
 import { TimerMode } from '@/helpers/timer/factory';
 import { serializeTimerConfig } from '@/helpers/timer/utils/config-serializer';
 import { convertTimeToMs } from '@/helpers/timer/utils/formatter';
 import { useCallback, useState } from 'react';
-import type { FC, PropsWithChildren } from 'react';
+import type { FC } from 'react';
+import Button from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
 
 const emomConfig: TimerConfig = {
   mode: TimerMode.EMOM,
@@ -52,60 +53,55 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Index Screen</Text>
+      <Text>Index Screen</Text>
       <View style={buttonsStyles.linkWrapper}>
-        <Text style={buttonsStyles.heading}>Jobs</Text>
-        <TimerLink config={emomConfig} inspect={inspectMode}>
-          EMOM
-        </TimerLink>
-        <TimerLink config={tabataConfig} inspect={inspectMode}>
-          TABATA
-        </TimerLink>
-        <TimerLink config={amrapConfig} inspect={inspectMode}>
-          AMRAP
-        </TimerLink>
-        <TimerLink config={onOffConfig} inspect={inspectMode}>
-          ON/OFF
-        </TimerLink>
-        <TimerLink config={quickOnOffConfig} inspect={inspectMode}>
-          QUICK ON/OFF
-        </TimerLink>
-        <TimerLink config={forTimeConfig} inspect={inspectMode}>
-          FOR TIME
-        </TimerLink>
+        <Text colorSubtone="500">Jobs</Text>
+        <TimerLink config={emomConfig} inspect={inspectMode} title="EMOM" />
+        <TimerLink config={tabataConfig} inspect={inspectMode} title="TABATA" />
+        <TimerLink config={amrapConfig} inspect={inspectMode} title="AMRAP" />
+        <TimerLink config={onOffConfig} inspect={inspectMode} title="ON_OFF" />
+        <TimerLink
+          config={quickOnOffConfig}
+          inspect={inspectMode}
+          title="QUICK ON_OFF"
+        />
+        <TimerLink
+          config={forTimeConfig}
+          inspect={inspectMode}
+          title="FOR_TIME"
+        />
+
         <View style={styles.optionsWrapper}>
-          <Text style={buttonsStyles.heading}>
+          <Text colorSubtone="500">
             Inspect mode screen: {inspectMode.toString()}
           </Text>
-          <Pressable
-            style={buttonsStyles.toggleInspectButton}
+          <Button
+            title="toggle inspect mode"
+            variant="outline"
+            size="sm"
             onPress={handleToggleInspectMode}
-          >
-            <Text style={buttonsStyles.toggleInspectButtonText}>
-              toggle inspect mode
-            </Text>
-          </Pressable>
+          />
         </View>
       </View>
     </View>
   );
 }
 
-interface TimerLinkProps extends PropsWithChildren {
+interface TimerLinkProps {
   config: TimerConfig;
   inspect?: boolean;
-  children: PropsWithChildren['children'] | string;
+  title: string;
 }
 
-const TimerLink: FC<TimerLinkProps> = ({ config, inspect, children }) => (
+const TimerLink: FC<TimerLinkProps> = ({ config, inspect, title }) => (
   <Link
     href={{
       params: serializeTimerConfig(config),
       pathname: inspect ? '/timer/inspector' : '/timer',
     }}
-    style={buttonsStyles.link}
+    asChild
   >
-    {children}
+    <Button title={title} variant="secondary" size="sm" />
   </Link>
 );
 
@@ -119,46 +115,12 @@ const styles = StyleSheet.create({
   optionsWrapper: {
     gap: 6,
   },
-  text: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist',
-    fontWeight: '400',
-  },
 });
 
 const buttonsStyles = StyleSheet.create({
-  heading: {
-    color: colors.neutral[500],
-    fontFamily: 'Geist',
-    fontWeight: '400',
-  },
-  link: {
-    backgroundColor: colors.neutral[800],
-    borderRadius: 10,
-    color: colors.neutral[400],
-    fontFamily: 'Geist',
-    fontSize: 18,
-    fontWeight: '400',
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    textAlign: 'center',
-  },
   linkWrapper: {
     gap: 8,
     justifyContent: 'center',
     width: '100%',
-  },
-  toggleInspectButton: {
-    borderColor: colors.neutral[500],
-    borderRadius: 10,
-    borderWidth: 1,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    textAlign: 'center',
-  },
-  toggleInspectButtonText: {
-    color: colors.neutral[500],
-    fontFamily: 'Geist',
-    fontWeight: '400',
   },
 });
