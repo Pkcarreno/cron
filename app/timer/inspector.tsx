83b40a9a88dc022e33d8ba712809b4c0e1104617
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-03-03 13:11:41 -0400

    fix: use custom text & button components

--- app/timer/inspector.tsx
+++ app/timer/inspector.tsx
@@ -1,4 +1,4 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { colors } from '@/helpers/colors';
 import { Link, useLocalSearchParams } from 'expo-router';
 import { TimerStatus, useTimer } from '@/hooks/use-timer';
@@ -8,6 +8,8 @@ import { useKeepAwake } from 'expo-keep-awake';
 import type { TimerPhase } from '@/helpers/timer/strategy';
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from '@/components/ui/text';
+import Button from '@/components/ui/button';
 
 interface logType {
   time: string;
@@ -58,103 +60,127 @@ export default function TimerInspectorScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.contentWrapper}>
-        <Text style={styles.text}>Timer Screen</Text>
+        <Text>Timer Screen</Text>
 
         <View style={timerStyles.container}>
-          <Text style={timerStyles.text}>{minutes}</Text>
-          <Text style={timerStyles.text}>:</Text>
-          <Text style={timerStyles.text}>{seconds}</Text>
+          <Text colorSubtone="200" size={24} weight="800" fontType="mono">
+            {minutes}
+          </Text>
+          <Text colorSubtone="200" size={24} weight="800" fontType="mono">
+            :
+          </Text>
+          <Text colorSubtone="200" size={24} weight="800" fontType="mono">
+            {seconds}
+          </Text>
         </View>
 
         <View style={dataBoxStyles.container}>
-          <Text style={dataBoxStyles.heading}>input</Text>
+          <Text>input</Text>
           <View style={dataBoxStyles.dataWrapper}>
             {Object.entries(timerInput).map(([key, value]) => (
               <View
                 key={`${key}-${value}`}
                 style={dataBoxStyles.dataElementWrapper}
               >
-                <Text style={dataBoxStyles.dataKeyText}>{key}</Text>
-                <Text style={dataBoxStyles.dataValueText}>{value}</Text>
+                <Text colorSubtone="600" fontType="mono">
+                  {key}
+                </Text>
+                <Text fontType="mono">{value}</Text>
               </View>
             ))}
           </View>
         </View>
 
         <View style={dataBoxStyles.container}>
-          <Text style={dataBoxStyles.heading}>state</Text>
+          <Text>state</Text>
           <View style={dataBoxStyles.dataWrapper}>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>modeAbbr</Text>
-              <Text style={dataBoxStyles.dataValueText}>{modeAbbr}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                modeAbbr
+              </Text>
+              <Text fontType="mono">{modeAbbr}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>status</Text>
-              <Text style={dataBoxStyles.dataValueText}>{status}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                status
+              </Text>
+              <Text fontType="mono">{status}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>phase</Text>
-              <Text style={dataBoxStyles.dataValueText}>{phase}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                phase
+              </Text>
+              <Text fontType="mono">{phase}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>totalRounds</Text>
-              <Text style={dataBoxStyles.dataValueText}>{totalRounds}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                totalRounds
+              </Text>
+              <Text fontType="mono">{totalRounds}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>currentRound</Text>
-              <Text style={dataBoxStyles.dataValueText}>{currentRound}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                currentRound
+              </Text>
+              <Text fontType="mono">{currentRound}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>displayTimeMs</Text>
-              <Text style={dataBoxStyles.dataValueText}>{displayTimeMs}</Text>
+              <Text colorSubtone="600" fontType="mono">
+                displayTimeMs
+              </Text>
+              <Text fontType="mono">{displayTimeMs}</Text>
             </View>
           </View>
         </View>
 
         <View style={dataBoxStyles.container}>
-          <Text style={dataBoxStyles.heading}>flags</Text>
+          <Text>flags</Text>
           <View style={dataBoxStyles.dataWrapper}>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>isPreparing</Text>
-              <Text style={dataBoxStyles.dataValueText}>
-                {flags.isPreparing.toString()}
+              <Text colorSubtone="600" fontType="mono">
+                isPreparing
               </Text>
+              <Text fontType="mono">{flags.isPreparing.toString()}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>showsPhaseIndicator</Text>
-              <Text style={dataBoxStyles.dataValueText}>
+              <Text colorSubtone="600" fontType="mono">
+                showsPhaseIndicator
+              </Text>
+              <Text fontType="mono">
                 {flags.showsPhaseIndicator.toString()}
               </Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>showsRoundCounter</Text>
-              <Text style={dataBoxStyles.dataValueText}>
-                {flags.showsRoundCounter.toString()}
+              <Text colorSubtone="600" fontType="mono">
+                showsRoundCounter
               </Text>
+              <Text fontType="mono">{flags.showsRoundCounter.toString()}</Text>
             </View>
             <View style={dataBoxStyles.dataElementWrapper}>
-              <Text style={dataBoxStyles.dataKeyText}>showsMinuteDigits</Text>
-              <Text style={dataBoxStyles.dataValueText}>
-                {flags.showsMinuteDigits.toString()}
+              <Text colorSubtone="600" fontType="mono">
+                showsMinuteDigits
               </Text>
+              <Text fontType="mono">{flags.showsMinuteDigits.toString()}</Text>
             </View>
           </View>
         </View>
 
         <View style={logStyles.container}>
-          <Text style={logStyles.heading}>logs</Text>
+          <Text size={16}>logs</Text>
           <ScrollView style={logStyles.logWrapper}>
             {eventLogs.map((log) => (
               <View
                 key={`${log.time}-${log.message}`}
                 style={logStyles.logItemWrapper}
               >
-                <Text style={logStyles.logTimeText}>[{log.time}]</Text>
-                <Text style={logStyles.logMessageText}>{log.message}</Text>
+                <Text colorSubtone="600" fontType="mono">
+                  [{log.time}]
+                </Text>
+                <Text fontType="mono">{log.message}</Text>
               </View>
             ))}
             {eventLogs.length === 0 && (
-              <Text style={logStyles.text}>No log yet</Text>
+              <Text colorSubtone="500">No log yet</Text>
             )}
           </ScrollView>
         </View>
@@ -162,49 +188,29 @@ export default function TimerInspectorScreen() {
 
       <View style={buttonsStyles.container}>
         <View style={buttonsStyles.actionWrapper}>
-          <Pressable onPress={start}>
-            <Text
-              style={
-                status === TimerStatus.READY || status === TimerStatus.PAUSED
-                  ? buttonsStyles.action
-                  : buttonsStyles.actionDisabled
-              }
-              disabled={
-                status === TimerStatus.READY || status === TimerStatus.PAUSED
-              }
-            >
-              Start
-            </Text>
-          </Pressable>
+          <Button
+            title="Start"
+            onPress={start}
+            disabled={
+              status !== TimerStatus.READY && status !== TimerStatus.PAUSED
+            }
+          />
 
-          <Pressable onPress={pause}>
-            <Text
-              style={
-                status === TimerStatus.RUNNING
-                  ? buttonsStyles.action
-                  : buttonsStyles.actionDisabled
-              }
-              disabled={status === TimerStatus.RUNNING}
-            >
-              Pause
-            </Text>
-          </Pressable>
+          <Button
+            title="Pause"
+            onPress={pause}
+            disabled={status !== TimerStatus.RUNNING}
+          />
 
-          <Pressable onPress={reset}>
-            <Text
-              style={
-                status === TimerStatus.READY
-                  ? buttonsStyles.actionDisabled
-                  : buttonsStyles.action
-              }
-              disabled={status !== TimerStatus.READY}
-            >
-              Reset
-            </Text>
-          </Pressable>
+          <Button
+            title="Reset"
+            onPress={reset}
+            disabled={status === TimerStatus.READY}
+          />
         </View>
-        <Link href=".." style={buttonsStyles.link}>
-          Back to Index
+
+        <Link href=".." asChild>
+          <Button title="Back to menu" variant="secondary" />
         </Link>
       </View>
     </SafeAreaView>
@@ -221,22 +227,11 @@ const styles = StyleSheet.create({
     flex: 1,
     gap: 6,
   },
-  text: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist',
-    fontWeight: '400',
-  },
 });
 
 const timerStyles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-  },
-  text: {
-    color: colors.neutral[200],
-    fontFamily: 'Geist Mono',
-    fontSize: 24,
-    fontWeight: '800',
   },
 });
 
@@ -248,24 +243,8 @@ const dataBoxStyles = StyleSheet.create({
     flexDirection: 'row',
     gap: 8,
   },
-  dataKeyText: {
-    color: colors.neutral[600],
-    fontFamily: 'Geist Mono',
-    fontWeight: '400',
-  },
-  dataValueText: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist Mono',
-    fontWeight: '400',
-  },
   dataWrapper: {
     paddingHorizontal: 8,
-  },
-  heading: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist',
-    fontSize: 16,
-    fontWeight: '400',
   },
 });
 
@@ -274,25 +253,9 @@ const logStyles = StyleSheet.create({
     height: 200,
     paddingVertical: 4,
   },
-  heading: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist',
-    fontSize: 16,
-    fontWeight: '400',
-  },
   logItemWrapper: {
     flexDirection: 'row',
     gap: 8,
-  },
-  logMessageText: {
-    color: colors.neutral[400],
-    fontFamily: 'Geist Mono',
-    fontWeight: '400',
-  },
-  logTimeText: {
-    color: colors.neutral[600],
-    fontFamily: 'Geist Mono',
-    fontWeight: '400',
   },
   logWrapper: {
     borderColor: colors.neutral[800],
@@ -302,36 +265,9 @@ const logStyles = StyleSheet.create({
     marginTop: 4,
     paddingHorizontal: 8,
   },
-  text: {
-    color: colors.neutral[500],
-    fontFamily: 'Geist Mono',
-    fontSize: 12,
-    fontWeight: '400',
-    marginBottom: 5,
-  },
 });
 
 const buttonsStyles = StyleSheet.create({
-  action: {
-    backgroundColor: colors.neutral[400],
-    borderRadius: 10,
-    color: colors.black,
-    fontFamily: 'Geist Mono',
-    fontSize: 18,
-    fontWeight: '600',
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-  },
-  actionDisabled: {
-    backgroundColor: colors.neutral[700],
-    borderRadius: 10,
-    color: colors.black,
-    fontFamily: 'Geist Mono',
-    fontSize: 18,
-    fontWeight: '600',
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-  },
   actionWrapper: {
     flexDirection: 'row',
     gap: 4,
@@ -339,15 +275,5 @@ const buttonsStyles = StyleSheet.create({
   },
   container: {
     gap: 16,
-  },
-  link: {
-    backgroundColor: colors.neutral[800],
-    borderRadius: 10,
-    color: colors.neutral[400],
-    fontFamily: 'Geist Mono',
-    fontSize: 18,
-    fontWeight: '400',
-    paddingHorizontal: 12,
-    paddingVertical: 8,
   },
 });
