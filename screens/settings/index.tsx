8f0e3fb728a366a8a57137028d685f45a7bea33f
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-03-15 15:44:49 -0400

    feat: add settings screen

---
+++ screens/settings/index.tsx
@@ -0,0 +1,129 @@
+import { NumberPickerSheet } from '@/components/input-number';
+import {
+  Menu,
+  MenuGroup,
+  MenuItem,
+  MenuItemText,
+  MenuPressableItem,
+  MenuTopic,
+} from '@/components/menu';
+import { Switch } from '@/components/switch';
+import { Text } from '@/components/text';
+import Env from '@/env';
+import { colors } from '@/helpers/colors';
+import { formatDuration } from '@/helpers/timer/utils/formatter';
+import { isRawTimerAtom, preparationTimeMsAtom } from '@/stores/settings';
+import { Link, Stack } from 'expo-router';
+import { useAtom } from 'jotai/react';
+import { CaretRightIcon } from 'phosphor-react-native';
+import { useCallback } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+export const Settings = () => {
+  const [isRawTimer, setIsRawTimer] = useAtom(isRawTimerAtom);
+  const [preparationTimeMs, setPreparationTimeMs] = useAtom(
+    preparationTimeMsAtom
+  );
+
+  const handlePreparationTimeChange = useCallback(
+    (value: number) => {
+      setPreparationTimeMs(value * 1000);
+    },
+    [setPreparationTimeMs]
+  );
+
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          title: 'Settings',
+        }}
+      />
+
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <Menu>
+          <MenuTopic>Preferences</MenuTopic>
+          <MenuGroup>
+            <NumberPickerSheet
+              value={preparationTimeMs / 1000}
+              min={1}
+              max={99}
+              valueSuffix="sec"
+              onChangeValue={handlePreparationTimeChange}
+            >
+              <MenuPressableItem>
+                <MenuItemText>Preparation</MenuItemText>
+                <MenuItemText colorSubtone="400">
+                  {formatDuration(preparationTimeMs)}
+                </MenuItemText>
+              </MenuPressableItem>
+            </NumberPickerSheet>
+          </MenuGroup>
+        </Menu>
+
+        <Menu>
+          <MenuTopic>About & Support</MenuTopic>
+          <MenuGroup>
+            <Link href="https://www.pkcarreno.com/projects/cron" asChild>
+              <MenuPressableItem>
+                <MenuItemText>About the Project</MenuItemText>
+                <CaretRightIcon size={14} color={colors.neutral[300]} />
+              </MenuPressableItem>
+            </Link>
+
+            <Link href="https://github.com/pkcarreno/cron/issues" asChild>
+              <MenuPressableItem>
+                <MenuItemText>Report an Issue</MenuItemText>
+                <CaretRightIcon size={14} color={colors.neutral[300]} />
+              </MenuPressableItem>
+            </Link>
+
+            <Link href="https://github.com/pkcarreno/cron" asChild>
+              <MenuPressableItem>
+                <MenuItemText>Source Code</MenuItemText>
+                <CaretRightIcon size={14} color={colors.neutral[300]} />
+              </MenuPressableItem>
+            </Link>
+          </MenuGroup>
+        </Menu>
+
+        <Menu>
+          <MenuTopic>Developer</MenuTopic>
+          <MenuGroup>
+            <MenuItem>
+              <MenuItemText>Enable Raw Timer</MenuItemText>
+              <Switch checked={isRawTimer} onCheckedChange={setIsRawTimer} />
+            </MenuItem>
+          </MenuGroup>
+        </Menu>
+
+        <View style={styles.extra}>
+          <Text colorSubtone="600" fontType="mono">
+            {Env.EXPO_PUBLIC_NAME} v{Env.EXPO_PUBLIC_VERSION}
+          </Text>
+          <Text colorSubtone="600" fontType="mono">
+            by pkcarreno
+          </Text>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  contentContainer: {
+    gap: 16,
+    paddingVertical: 12,
+  },
+  extra: {
+    paddingHorizontal: 12,
+  },
+});
