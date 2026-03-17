ce3c68a0947793189cbec0e58d0b0c95ea29bf69
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-03-16 21:41:44 -0400

    feat: add sound & haptic switches + make each item a component with is own logic encapsulated

--- screens/settings/index.tsx
+++ screens/settings/index.tsx
@@ -13,7 +13,9 @@ import Env from '@/env';
 import { colors } from '@/helpers/colors';
 import { formatDuration } from '@/helpers/timer/utils/formatter';
 import {
+  hasHapticEnabledAtom,
   isRawTimerEnabledAtom,
+  isSoundEnabledAtom,
   preparationTimeMsAtom,
 } from '@/stores/settings';
 import { Link, Stack } from 'expo-router';
@@ -22,10 +24,75 @@ import { CaretRightIcon } from 'phosphor-react-native';
 import { useCallback } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 
-export const Settings = () => {
-  const [isRawTimerEnabled, setIsRawTimerEnabled] = useAtom(
-    isRawTimerEnabledAtom
-  );
+export const Settings = () => (
+  <>
+    <Stack.Screen
+      options={{
+        headerShown: true,
+        title: 'Settings',
+      }}
+    />
+
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      contentInsetAdjustmentBehavior="automatic"
+    >
+      <Menu>
+        <MenuTopic>Preferences</MenuTopic>
+        <MenuGroup>
+          <PreparationSelectorItem />
+          <SoundSwitchItem />
+          <HapticsSwitchItem />
+        </MenuGroup>
+      </Menu>
+
+      <Menu>
+        <MenuTopic>About & Support</MenuTopic>
+        <MenuGroup>
+          <Link href="https://www.pkcarreno.com/projects/cron" asChild>
+            <MenuPressableItem>
+              <MenuItemText>About the Project</MenuItemText>
+              <CaretRightIcon size={14} color={colors.neutral[300]} />
+            </MenuPressableItem>
+          </Link>
+
+          <Link href="https://github.com/pkcarreno/cron/issues" asChild>
+            <MenuPressableItem>
+              <MenuItemText>Report an Issue</MenuItemText>
+              <CaretRightIcon size={14} color={colors.neutral[300]} />
+            </MenuPressableItem>
+          </Link>
+
+          <Link href="https://github.com/pkcarreno/cron" asChild>
+            <MenuPressableItem>
+              <MenuItemText>Source Code</MenuItemText>
+              <CaretRightIcon size={14} color={colors.neutral[300]} />
+            </MenuPressableItem>
+          </Link>
+        </MenuGroup>
+      </Menu>
+
+      <Menu>
+        <MenuTopic>Developer</MenuTopic>
+        <MenuGroup>
+          <RawTimerSwitchItem />
+        </MenuGroup>
+      </Menu>
+
+      <View style={styles.extra}>
+        <Text colorSubtone="600" fontType="mono">
+          {Env.EXPO_PUBLIC_NAME} v{Env.EXPO_PUBLIC_VERSION}
+        </Text>
+        <Text colorSubtone="600" fontType="mono">
+          by pkcarreno
+        </Text>
+      </View>
+    </ScrollView>
+  </>
+);
+
+const PreparationSelectorItem = () => {
   const [preparationTimeMs, setPreparationTimeMs] = useAtom(
     preparationTimeMsAtom
   );
@@ -38,90 +105,63 @@ export const Settings = () => {
   );
 
   return (
-    <>
-      <Stack.Screen
-        options={{
-          headerShown: true,
-          title: 'Settings',
-        }}
+    <NumberPickerSheet
+      value={preparationTimeMs / 1000}
+      min={1}
+      max={99}
+      valueSuffix="sec"
+      onChangeValue={handlePreparationTimeChange}
+    >
+      {(triggerProps) => (
+        <MenuPressableItem {...triggerProps}>
+          <MenuItemText>Preparation</MenuItemText>
+          <MenuItemText colorSubtone="400">
+            {formatDuration(preparationTimeMs)}
+          </MenuItemText>
+        </MenuPressableItem>
+      )}
+    </NumberPickerSheet>
+  );
+};
+
+const RawTimerSwitchItem = () => {
+  const [isRawTimerEnabled, setIsRawTimerEnabled] = useAtom(
+    isRawTimerEnabledAtom
+  );
+
+  return (
+    <MenuItem>
+      <MenuItemText>Debug Timer</MenuItemText>
+      <Switch
+        checked={isRawTimerEnabled}
+        onCheckedChange={setIsRawTimerEnabled}
       />
+    </MenuItem>
+  );
+};
 
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-        contentInsetAdjustmentBehavior="automatic"
-      >
-        <Menu>
-          <MenuTopic>Preferences</MenuTopic>
-          <MenuGroup>
-            <NumberPickerSheet
-              value={preparationTimeMs / 1000}
-              min={1}
-              max={99}
-              valueSuffix="sec"
-              onChangeValue={handlePreparationTimeChange}
-            >
-              {(triggerProps) => (
-                <MenuPressableItem {...triggerProps}>
-                  <MenuItemText>Preparation</MenuItemText>
-                  <MenuItemText colorSubtone="400">
-                    {formatDuration(preparationTimeMs)}
-                  </MenuItemText>
-                </MenuPressableItem>
-              )}
-            </NumberPickerSheet>
-          </MenuGroup>
-        </Menu>
+const SoundSwitchItem = () => {
+  const [isSoundEnabled, setIsSoundEnabled] = useAtom(isSoundEnabledAtom);
 
-        <Menu>
-          <MenuTopic>About & Support</MenuTopic>
-          <MenuGroup>
-            <Link href="https://www.pkcarreno.com/projects/cron" asChild>
-              <MenuPressableItem>
-                <MenuItemText>About the Project</MenuItemText>
-                <CaretRightIcon size={14} color={colors.neutral[300]} />
-              </MenuPressableItem>
-            </Link>
+  return (
+    <MenuItem>
+      <MenuItemText>Sounds</MenuItemText>
+      <Switch checked={isSoundEnabled} onCheckedChange={setIsSoundEnabled} />
+    </MenuItem>
+  );
+};
 
-            <Link href="https://github.com/pkcarreno/cron/issues" asChild>
-              <MenuPressableItem>
-                <MenuItemText>Report an Issue</MenuItemText>
-                <CaretRightIcon size={14} color={colors.neutral[300]} />
-              </MenuPressableItem>
-            </Link>
+const HapticsSwitchItem = () => {
+  const [hasHapticEnabled, setHasHapticEnabled] = useAtom(hasHapticEnabledAtom);
 
-            <Link href="https://github.com/pkcarreno/cron" asChild>
-              <MenuPressableItem>
-                <MenuItemText>Source Code</MenuItemText>
-                <CaretRightIcon size={14} color={colors.neutral[300]} />
-              </MenuPressableItem>
-            </Link>
-          </MenuGroup>
-        </Menu>
-
-        <Menu>
-          <MenuTopic>Developer</MenuTopic>
-          <MenuGroup>
-            <MenuItem>
-              <MenuItemText>Enable Raw Timer</MenuItemText>
-              <Switch
-                checked={isRawTimerEnabled}
-                onCheckedChange={setIsRawTimerEnabled}
-              />
-            </MenuItem>
-          </MenuGroup>
-        </Menu>
-
-        <View style={styles.extra}>
-          <Text colorSubtone="600" fontType="mono">
-            {Env.EXPO_PUBLIC_NAME} v{Env.EXPO_PUBLIC_VERSION}
-          </Text>
-          <Text colorSubtone="600" fontType="mono">
-            by pkcarreno
-          </Text>
-        </View>
-      </ScrollView>
-    </>
+  return (
+    <MenuItem>
+      <MenuItemText>Haptics</MenuItemText>
+      <Switch
+        checked={hasHapticEnabled}
+        onCheckedChange={setHasHapticEnabled}
+      />
+    </MenuItem>
   );
 };
 
