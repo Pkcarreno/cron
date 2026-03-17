689ce9013551c217a8ff6926374132a3e83564f6
Pedro Carreno <34664891+Pkcarreno@users.noreply.github.com>
2026-03-16 21:40:03 -0400

    feat: add sound & haptics relate states

--- stores/settings.ts
+++ stores/settings.ts
@@ -5,6 +5,16 @@ export const isRawTimerEnabledAtom = atomWithMMKV<boolean>(
   false
 );
 
+export const hasHapticEnabledAtom = atomWithMMKV<boolean>(
+  'settings.hasHapticEnabled',
+  true
+);
+
+export const isSoundEnabledAtom = atomWithMMKV<boolean>(
+  'settings.isSoundEnabled',
+  true
+);
+
 export const preparationTimeMsAtom = atomWithMMKV<number>(
   'settings.preparationTimeMs',
   10_000
